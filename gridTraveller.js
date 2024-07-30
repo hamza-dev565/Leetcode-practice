87eca1e2e769462db3ddc89be7b515536a510bd1
eng-hamza01 <hamzan.naseer@digitify.com>
2024-07-30 11:42:56 +0500

    Tested zsh again

---
+++ gridTraveller.js
@@ -0,0 +1,5 @@
+// let say we have a 3x3 grid and we want to find the ways to travel to some other point.
+// we have  some base cases lets say gridTraveller(2,0) will be the number of ways to travel will be 0 same for 0,1 - ,1,0 8,0 is 0
+// for 1x1 its 1
+
+// for a 2x3 we will have to go downward lets think of as tree for left child we can decrease the row and for right we can decrease column
